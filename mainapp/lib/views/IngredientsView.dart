0892c7d51f6dfd0795c0f09900662c2896a106e9
mg <morgoth2006@gmail.com>
2021-09-06 17:48:09 +0300

    ui development. CSV parsing

--- mainapp/lib/views/IngredientsView.dart
+++ mainapp/lib/views/IngredientsView.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-class InredientsView extends StatelessWidget
+class IngredientsView extends StatelessWidget
 {
   @override
   Widget build(BuildContext context) {
